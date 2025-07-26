89b1a2cfc19efd7a9b3fbc171287f83d319095bf
Wasit Mirani <44178287+wasitmirani@users.noreply.github.com>
2025-07-27 01:59:04 +0500

    update changes

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,8 +1,22 @@
 package main
 
 
+import (
+	"fmt"
+	"net/http"
+)
 
 func main(){
+	fmt.Println("Welcome to the API server!")
 
-	
+	// Initialize the API server
+	initializeAPIServer()
+}
+
+func initializeAPIServer() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, you've reached the API server!")
+	})
+
+	http.ListenAndServe(":8080", nil)
 }
